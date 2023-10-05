1279f7dc4b9d40c0374ed8bcb2db619e0530b8ec
Engombe Lokanga <engombeel13@gmail.com>
2023-10-05 20:40:23 +0100

    Created a sitemap

---
+++ services/sitemaps.py
@@ -0,0 +1,24 @@
+from django.contrib.sitemaps import Sitemap
+from .models import DigitalService, MarketingService
+
+class DigitalServiceSitemap(Sitemap):
+  changefreq = "weekly"
+  priority = 0.8
+  protocol = 'https'
+
+  def items(self):
+    return DigitalService.objects.all()
+  
+  def location(self):
+    return 'services/digital'
+
+class MarketingServiceSitemap(Sitemap):
+  changefreq = "weekly"
+  priority = 0.8
+  protocol = 'https'
+
+  def items(self):
+    return MarketingService.objects.all()
+  
+  def location(self):
+    return 'services/marketing'
--- services/urls.py
+++ services/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from services import views
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import DigitalServiceSitemap, MarketingServiceSitemap
+
+sitemaps = {
+  'services': DigitalServiceSitemap,
+  'services': MarketingServiceSitemap
+}
 
 urlpatterns = [
     path("", views.hodlok_services, name='services'),
     path("digital/", views.digital_services, name='digital'),
     path("marketing/", views.marketing_services, name='marketing'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemap}, name='django.contrib.sitemaps.views.sitemap'),
 ]
