c2d0b33358a6072348677a29ca038e0d8b7d5c19
Engombe Lokanga <engombeel13@gmail.com>
2023-10-05 22:22:49 +0100

    Added robots.txt file

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from home import views
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import StaticViewSitemap
+from django.views.generic.base import TemplateView
 
 sitemaps = {
   'static':StaticViewSitemap
@@ -11,4 +12,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('privacy/', views.privacy, name='privacy'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
