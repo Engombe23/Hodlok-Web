3c823bd2ee7df24e710c231e37c0896fd9b56141
Engombe Lokanga <engombeel13@gmail.com>
2023-10-05 22:25:07 +0100

    Added robots.txt file

--- home/urls.py
+++ home/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('privacy/', views.privacy, name='privacy'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
-    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    path("robots.txt", TemplateView.as_view(template_name="home/robots.txt", content_type="text/plain")),
 ]
