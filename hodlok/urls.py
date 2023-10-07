ae1db51f0db3f14e4fb4411e0695707e9b7ecdc5
Engombe Lokanga <engombeel13@gmail.com>
2023-10-07 22:20:24 +0100

    Removed Django SEO URL path

--- hodlok/urls.py
+++ hodlok/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("django-check-seo/", include("django_check_seo.urls")),
     path('', include('home.urls')),
     path('services/', include('services.urls')),
     path('contacts/', include('contacts.urls')),
